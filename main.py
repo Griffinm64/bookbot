7d8ab2e96e2446ee2b99f1f8a78439cbb2721382
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-06-05 03:19:48 -0400

    generated report foprmat

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
-from stats import count_book_words
+from stats import count_book_words, count_book_letters, sorted_dict_list
 
 def main():
-    print(f"{count_book_words("./books/frankenstein.txt")} words found in the document")
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ---------")
+    print(f"Found {count_book_words(
+        "./books/frankenstein.txt")} total words")
+    print("--------- Character Count -------")
+    #print(f"{sorted_dict_list(("./books/frankenstein.txt"))}")
+    for k,v in sorted_dict_list("./books/frankenstein.txt"):
+        if k.isalpha(): print(f"{k}: {v}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,22 @@ def count_book_words(file_path):
     for word in book:
         num_words += 1
     return num_words
+
+def count_book_letters(file_path):
+    num_letters = {}
+    with open(file_path) as f:
+        book = f.read().split()
+    for word in book:
+        for letter in word:
+            if letter.lower() in num_letters:
+                num_letters[letter.lower()] += 1
+            else:
+                num_letters[letter.lower()] = 1
+    return num_letters
+
+
+def sorted_dict_list(file_path):
+    dict = count_book_letters(file_path)
+    return sorted(
+        dict.items(),key=lambda item:
+        item[1], reverse=True)
