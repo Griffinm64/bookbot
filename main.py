08abb436d37ff4ec76038d792f54cca8291f1608
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-06-05 04:59:13 -0400

    Added sys.argv functionality, added file path

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 
 from stats import count_book_words, count_book_letters, sorted_dict_list
+import sys
+
+#Checks for required arguments (2)
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_file_path = sys.argv[1]
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_file_path}")
     print("----------- Word Count ---------")
     print(f"Found {count_book_words(
-        "./books/frankenstein.txt")} total words")
+        book_file_path)} total words")
     print("--------- Character Count -------")
-    #print(f"{sorted_dict_list(("./books/frankenstein.txt"))}")
-    for k,v in sorted_dict_list("./books/frankenstein.txt"):
+
+    for k,v in sorted_dict_list(book_file_path):
         if k.isalpha(): print(f"{k}: {v}")
     print("============= END ===============")
 
