05c3643dc08ccc0afa6f4fed333fe818cee82752
griffinm64 <61828980+Griffinm64@users.noreply.github.com>
2025-06-06 03:41:50 -0400

    Added common word count, needs further refinement

--- stats.py
+++ stats.py
@@ -6,6 +6,17 @@ def count_book_words(file_path):
         num_words += 1
     return num_words
 
+def common_book_words(file_path):
+    dict_words = {}
+    with open(file_path) as f:
+        book = f.read().split()
+    for word in book:
+            if word.lower() in dict_words:
+                dict_words[word.lower()] += 1
+            else:
+                dict_words[word.lower()] = 1
+    return dict_words
+
 def count_book_letters(file_path):
     num_letters = {}
     with open(file_path) as f:
@@ -21,6 +32,12 @@ def count_book_letters(file_path):
 
 def sorted_dict_list(file_path):
     dict = count_book_letters(file_path)
+    return sorted(
+        dict.items(),key=lambda item:
+        item[1], reverse=True)
+
+def sorted_dict_word_list(file_path):
+    dict = common_book_words(file_path)
     return sorted(
         dict.items(),key=lambda item:
         item[1], reverse=True)
